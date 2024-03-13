65c0b19fef90a4a80e495e6a392e824145e62598
窦维 <15951653662@163.com>
2024-03-13 15:41:27 -0400

    2024-03-13

---
+++ 264/Solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        heap = [1]
+        count = 1
+        while count <= n:
+            target = heapq.heappop(heap)
+            if target*2 not in heap:
+                heapq.heappush(heap,target*2)
+            if target*3 not in heap:
+                heapq.heappush(heap,target*3)
+            if target*5 not in heap:
+                heapq.heappush(heap,target*5)
+            count += 1
+        return target
