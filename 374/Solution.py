a38a740bb05403f5bee49d7eab113ff4065bdd31
窦维 <15951653662@163.com>
2024-03-17 14:32:08 -0400

    2024-03-17

--- 374/Solution.py
+++ 374/Solution.py
@@ -20,4 +20,18 @@ class Solution:
             else:
                 pass
         return -1
+
+################################        
+        left,right=1,n
+        mid=left
+        while left<=right:
+            mid=left+(right-left)//2
+            res=guess(mid)
+            if res==0:
+                return mid
+            elif res==1:
+                left=mid+1
+            elif res==-1:
+                right=mid-1
         
+        return mid
