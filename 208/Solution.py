ce6ab20386a20b6a831e89bf078d2b8f08ab2f47
窦维 <15951653662@163.com>
2024-06-03 11:45:21 -0400

    2024-06-03

---
+++ 208/Solution.py
@@ -0,0 +1,75 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+
+        return cur.endOfWord
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+
+# My way
+class Trie:
+
+    def __init__(self):
+        self.trie = []
+
+    def insert(self, word: str) -> None:
+        if word not in self.trie:
+            self.trie.append(word)
+        return 
+        
+
+    def search(self, word: str) -> bool:
+        if word in self.trie:
+            return True
+        return False
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        for s in self.trie:
+            if s[:len(prefix)] == prefix:
+                return True
+        return False
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
