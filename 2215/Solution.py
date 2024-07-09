7db8382916448ba2051d5b62de8eb796a7e94e17
窦维 <15951653662@163.com>
2024-07-09 13:11:48 -0400

    2024-07-09

---
+++ 2215/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        diff1, diff2 = set(), set()
+        for num in nums1:
+            if num not in nums2:
+                diff1.add(num)
+
+        for num in nums2:
+            if num not in nums1:
+                diff2.add(num)
+
+        return [list(diff1), list(diff2)]
